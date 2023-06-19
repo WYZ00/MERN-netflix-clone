d4420782e646ae32ee93d3616f9a2ec916adf633
WYZ00 <thanthtettin@gmail.com>
2023-06-19 21:24:34 +0630

    Navbar and Logout Add

--- netflix-ui/src/pages/Netflix.jsx
+++ netflix-ui/src/pages/Netflix.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
 
 const Netflix = () => {
-  return (
-    <div>Netflix</div>
-  )
-}
+  const [isScrolled, setIsScrolled] = useState(false);
 
-export default Netflix
+  useEffect(() => {
+    window.onscroll = () => {
+      setIsScrolled(window.pageYOffset === 0 ? false : true);
+    };
+    return () => (window.onscroll = null);
+  }, []);
+
+  return <div>
+    <Navbar isScrolled={isScrolled}/>
+  </div>;
+};
+
+export default Netflix;
